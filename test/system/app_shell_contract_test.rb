907c4593610af40f543be8b42c2ad60f55c466c1
tswarren <42260789+tswarren@users.noreply.github.com>
2026-07-06 12:29:41 -0400

    Harden app shell flash and disabled nav tests

--- test/system/app_shell_contract_test.rb
+++ test/system/app_shell_contract_test.rb
@@ -32,10 +32,29 @@ class AppShellContractTest < ApplicationSystemTestCase
     assert_selector ".ss-pos-workspace-header[aria-label='Point of Sale workspace']"
   end
 
+  test "permission-gated nav items expose disabled state" do
+    user = create_user!(username: "restricted_#{SecureRandom.hex(4)}")
+    store = create_store!
+    workstation = create_workstation!(store: store)
+
+    system_login!(user, workstation: workstation)
+    visit root_path
+
+    assert_selector "nav.ss-nav .ss-nav__item--disabled[aria-disabled='true']", text: "POS"
+    assert_selector "nav.ss-nav .ss-nav__item--disabled[aria-disabled='true']", text: "Inventory"
+    assert_no_selector "nav.ss-nav a", text: "POS"
+  end
+
   test "flash messages can be dismissed" do
     setup_pos_system_sale!
+    visit root_path
 
-    assert_selector ".ss-flash", wait: 5
+    all(".ss-flash__dismiss[aria-label='Dismiss message']", minimum: 0).each(&:click)
+
+    find(".ss-dropdown-trigger", text: @cashier.display_name).click
+    click_button "Compact View"
+
+    assert_selector ".ss-flash", text: "Appearance updated.", wait: 5
     assert_selector ".ss-flash__dismiss[aria-label='Dismiss message']"
 
     find(".ss-flash__dismiss[aria-label='Dismiss message']").click
